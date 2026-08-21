986c366c5c2daf80b4ed3f86750aeb1a552c9040
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-08-21 20:00:48 +0000

    chore(release): prepare v1.2.8

--- app/src/test/java/com/yugahashimoto/andcode/ReleaseMetadataTest.kt
+++ app/src/test/java/com/yugahashimoto/andcode/ReleaseMetadataTest.kt
@@ -10,7 +10,7 @@ class ReleaseMetadataTest {
         val releaseVersion = repositoryRoot().resolve(".release-version").readText().trim().removePrefix("v")
 
         assertEquals(releaseVersion, BuildConfig.VERSION_NAME)
-        assertEquals(46, BuildConfig.VERSION_CODE)
+        assertEquals(47, BuildConfig.VERSION_CODE)
     }
 
     private fun repositoryRoot(): File {
