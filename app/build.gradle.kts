d4df5c7e68b5b826d0847c69fa3b11f29a45289b
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-08-23 02:41:01 +0000

    chore(release): prepare v1.2.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = "com.yugahashimoto.andcode"
         minSdk = 26
         targetSdk = 35
-        versionCode = 48
-        versionName = "1.2.9"
+        versionCode = 49
+        versionName = "1.2.10"
         buildConfigField("String", "GITHUB_CLIENT_ID", "\"$githubClientId\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
